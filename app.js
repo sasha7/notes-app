f30af9a4d9e07d274fcc6ab93b27ab349fd6898e
Sasa Macakanja <sasa.macakanja@gmail.com>
2017-01-12 14:38:08 +0100

    rotate http req access logs daily with file-stream-rotator and morgan logger

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const hbs = require('hbs');
 const fs = require('fs');
+const rotator = require('file-stream-rotator');
 const hbsHelpers = require('./helpers/handlebars');
 
 const index = require('./routes/index');
@@ -19,7 +20,20 @@ hbsHelpers(hbs);
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // create a write stream in append mode
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flag: 'a' });
+// const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flag: 'a' });
+
+// setup log rotator
+let accessLogStreamRot;
+
+if (process.env.REQUEST_LOG_FILE) {
+  const logDir = path.dirname(process.env.REQUEST_LOG_FILE);
+  fs.existsSync(logDir) || fs.mkdirSync(logDir);
+  accessLogStreamRot = rotator.getStream({
+    filename: process.env.REQUEST_LOG_FILE,
+    frequency: 'daily',
+    verbose: false
+  });
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,10 +41,17 @@ app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger(process.env.REQUEST_LOG_FORMAT || 'dev'));
+// app.use(logger(process.env.REQUEST_LOG_FORMAT || 'dev'));
 
 // setup logger and log only error responses to access.log - without log rotation
-app.use(logger('combined', { stream: accessLogStream, skip: (req, res) => res.statusCode < 400 }));
+// app.use(logger('combined', { stream: accessLogStream, skip: (req, res) => res.statusCode < 400 }));
+
+
+// setup logger and log only error responses to access.log - with log rotation
+app.use(logger(process.env.REQUEST_LOG_FORMAT || 'dev', {
+  stream: accessLogStreamRot || process.stdout,
+  skip: (req, res) => res.statusCode < 400
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
