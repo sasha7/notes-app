b842f83c4ceb82ccc17968cea8694961b58f4a25
Sasa Macakanja <sasa.macakanja@gmail.com>
2017-01-27 00:01:15 +0100

    dumped latest sql from postgres

--- notes-db.sql
+++ notes-db.sql
@@ -27,6 +27,20 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
+--
+-- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+
+
 SET search_path = public, pg_catalog;
 
 SET default_tablespace = '';
@@ -34,45 +48,85 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: Notes; Type: TABLE; Schema: public; Owner: postgres
+-- Name: knex_migrations; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE "Notes" (
-    id uuid NOT NULL,
+CREATE TABLE knex_migrations (
+    id integer NOT NULL,
+    name character varying(255),
+    batch integer,
+    migration_time timestamp with time zone
+);
+
+
+ALTER TABLE knex_migrations OWNER TO postgres;
+
+--
+-- Name: knex_migrations_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE knex_migrations_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE knex_migrations_id_seq OWNER TO postgres;
+
+--
+-- Name: knex_migrations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE knex_migrations_id_seq OWNED BY knex_migrations.id;
+
+
+--
+-- Name: knex_migrations_lock; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE knex_migrations_lock (
+    is_locked integer
+);
+
+
+ALTER TABLE knex_migrations_lock OWNER TO postgres;
+
+--
+-- Name: notes; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE notes (
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
     title character varying(255),
-    body character varying(255),
-    created_at timestamp with time zone NOT NULL,
-    updated_at timestamp with time zone NOT NULL
+    body character varying(255)
 );
 
 
-ALTER TABLE "Notes" OWNER TO postgres;
+ALTER TABLE notes OWNER TO postgres;
 
 --
--- Name: SequelizeMeta; Type: TABLE; Schema: public; Owner: postgres
+-- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
-CREATE TABLE "SequelizeMeta" (
-    name character varying(255) NOT NULL
-);
-
-
-ALTER TABLE "SequelizeMeta" OWNER TO postgres;
-
---
--- Name: Notes_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY "Notes"
-    ADD CONSTRAINT "Notes_pkey" PRIMARY KEY (id);
+ALTER TABLE ONLY knex_migrations ALTER COLUMN id SET DEFAULT nextval('knex_migrations_id_seq'::regclass);
 
 
 --
--- Name: SequelizeMeta_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: knex_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY "SequelizeMeta"
-    ADD CONSTRAINT "SequelizeMeta_pkey" PRIMARY KEY (name);
+ALTER TABLE ONLY knex_migrations
+    ADD CONSTRAINT knex_migrations_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: notes_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY notes
+    ADD CONSTRAINT notes_pkey PRIMARY KEY (id);
 
 
 --
