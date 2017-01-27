9e1350fc83b33be1f6bd7e19522c78de6bbe47c5
Sasa Macakanja <sasa.macakanja@gmail.com>
2017-01-27 19:23:11 +0100

    reset config.js

--- config/config.js
+++ config/config.js
@@ -1,23 +1,11 @@
 module.exports = {
   development: {
-    username: 'postgres',
-    password: 'postgres',
-    database: 'notes_development',
-    host: '127.0.0.1',
-    dialect: 'postgres'
+
   },
   test: {
-    username: 'postgres',
-    password: 'postgres',
-    database: 'notes_test',
-    host: '127.0.0.1',
-    dialect: 'postgres'
+
   },
   production: {
-    username: 'postgres',
-    password: 'postgres',
-    database: 'notes_production ',
-    host: '127.0.0.1',
-    dialect: 'postgres'
+
   }
 };
