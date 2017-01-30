37becd4e7cf99cb462a8802e7f5af6cd4549a794
Sasa Macakanja <sasa.macakanja@gmail.com>
2017-01-30 11:19:30 +0100

    added model and db validation handler

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const hbs = require('hbs');
 const fs = require('fs');
-const rotator = require('file-stream-rotator');
 const hbsHelpers = require('./helpers/handlebars');
 const error = require('debug')('notes-app:error');
 const dotenv = require('dotenv');
@@ -20,11 +19,15 @@ const FileStore = require('session-file-store')(session);
 const passport = require('passport');
 const authentication = require('./authentication');
 const ensureAuthenticated = require('./authentication/middleware');
+const expressValidator = require('express-validator');
+const helpers = require('./lib/helpers');
+const parseUniqueViolationError = helpers.parseUniqueViolationError;
+const isPostgresError = helpers.isPostgresError;
 
 dotenv.load();
 
 // Initialize knex.
-const knex = Knex(knexConfig.development);
+const knex = Knex(knexConfig);
 
 // Bind all models to a knex instance (for only one database).
 // If there is more than one database, use Model.bindKnex method.
@@ -44,59 +47,46 @@ authentication.init(app);
 hbsHelpers(hbs);
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
-// create a write stream in append mode
-// const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flag: 'a' });
-
-// setup log rotator
-let accessLogStreamRot;
-
-if (process.env.REQUEST_LOG_FILE) {
-  const logDir = path.dirname(process.env.REQUEST_LOG_FILE);
-  fs.existsSync(logDir) || fs.mkdirSync(logDir);
-  accessLogStreamRot = rotator.getStream({
-    filename: process.env.REQUEST_LOG_FILE,
-    frequency: 'daily',
-    verbose: false
-  });
-}
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.disable('x-powered-by');
+
+if (app.get('env') === 'development') {
+  app.use(logger(process.env.REQUEST_LOG_FORMAT || 'dev'));
+}
+
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger(process.env.REQUEST_LOG_FORMAT || 'dev'));
-
-// setup logger and log only error responses to access.log - without log rotation
-// app.use(logger('combined', {
-//  stream: accessLogStream,
-//  skip: (req, res) => res.statusCode < 400
-// }));
-
-
-// setup logger and log only error responses to access.log - with log rotation
-// app.use(logger(process.env.REQUEST_LOG_FORMAT || 'dev', {
-//   stream: accessLogStreamRot || process.stdout,
-//   skip: (req, res) => res.statusCode < 400
-// }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// An express.js middleware for node-validator.
+app.use(expressValidator());
+
 app.use(cookieParser());
+
+// Server files in public
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Initialize session filestore
 app.use(session({
   store: new FileStore({ path: './sessions' }),
-  secret: 'Sn3q7Le0P5q|w_tRU&tJ@+e(&Lfj9Pn',
+  secret: process.env.SECRET_KEY,
   resave: true,
   saveUninitialized: true
 }));
+
+// Use flash messages
 app.use(flash());
+
+// Initialize passport to use sessions
 app.use(passport.initialize());
 app.use(passport.session());
 
-/**
- * If user is authenticated, set info for templates
- */
+
+// If user is authenticated, set local vars for templates
 app.use((req, res, next) => {
   if (req.user) {
     res.locals.isAuthenticated = true;
@@ -121,10 +111,25 @@ app.use((req, res, next) => {
 // error handler
 app.use((err, req, res, next) => {
   error(`${(err.status || err.statusCode || 500)} ${err.message || err.data || {}}`);
-  // handle validation errors from Objection.js ORM
+
+  // Handle validation errors from Objection.js ORM
   if (err.constructor.name === 'ValidationError') {
-    res.status(err.statusCode || err.status || 500).json(err.data || {});
-  } else {
+    error('ORM VALIDATION ERRORS');
+
+    res.status(err.statusCode || err.status || 500).json({
+      error_type: 'validation',
+      errors: err.data || {}
+    });
+  } else if (isPostgresError(err)) {
+    // Handle unique violation constraint errors from the Postgresql database itself
+    error('DATABASE UNIQUE VIOLATION ERROR');
+
+    const uniqueViolationErrors = parseUniqueViolationError(err);
+    res.status(400).json({
+      error_type: 'validation',
+      errors: uniqueViolationErrors || {}
+    });
+  } else { // Render 500 error as a html page
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -135,11 +140,11 @@ app.use((err, req, res, next) => {
   }
 });
 
-// handle uncaught exceptions - DO NOT USE IN PROD
-// if (app.get('env') === 'development') {
-//   process.on('uncaughtException', (err) => {
-//     error(`App crashed!! - ${(err.stack || err)}`);
-//   });
-// }
+// handle uncaught exceptions - ONLY IN DEVELOPMENT
+if (app.get('env') === 'development') {
+  process.on('uncaughtException', (err) => {
+    error(`App crashed!! - ${(err.stack || err)}`);
+  });
+}
 
 module.exports = app;
